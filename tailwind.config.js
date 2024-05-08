d6790bfefd84888a62bd0c3d0c272052276d8d5d
ronak204 <chavadaronak001>
2024-05-08 14:38:33 +0530

    tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*"],
+  theme: {
+    fontFamily: {
+      'Nunito Sans': ['sans-serif'],
+      'body': ['"Nunito Sans"'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
